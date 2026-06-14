5a5a3aa85691ae904f887dfd9728fe90852a0b0a
senipe <106387526+senipe@users.noreply.github.com>
2026-06-14 12:09:24 +0100

    updated gallery

--- images-manifest.js
+++ images-manifest.js
@@ -10,21 +10,22 @@ window.IMAGES_MANIFEST = {
     { "src": "images/hero/2.jpg", "thumb": "images/my-work/2_thumb.jpg", "x": 75 }
   ],
   "gallery": [
-    { "src": "images/my-work/1.jpg", "thumb": "images/my-work/1_thumb.jpg", "x": 50, "y": 100, "showOnMobileHome": true, "showOnPcHome": true },
-    { "src": "images/my-work/3.webp", "x": 50, "y": 65, "showOnMobileHome": true, "showOnPcHome": true },
-    { "src": "images/my-work/4.jpg", "x": 50, "y": 0, "showOnMobileHome": true, "showOnPcHome": true },
+    { "src": "images/my-work/16.jpg", "thumb": "images/my-work/16_thumb.jpg", "x": 50, "y": 100, "showOnMobileHome": true, "showOnPcHome": true },
+    { "src": "images/my-work/1.jpg", "thumb": "images/my-work/1_thumb.jpg", "x": 50, "y": 100, "showOnMobileHome": false, "showOnPcHome": false },
+    { "src": "images/my-work/3.webp", "x": 50, "y": 60, "showOnMobileHome": true, "showOnPcHome": true },
+    { "src": "images/my-work/4.jpg", "x": 50, "y": 15, "showOnMobileHome": true, "showOnPcHome": true },
     { "src": "images/my-work/2.jpg", "thumb": "images/my-work/2_thumb.jpg", "x": 100, "y": 50, "showOnMobileHome": true, "showOnPcHome": true },
     { "src": "images/my-work/5.jpg", "x": 50, "y": 0, "showOnMobileHome": false, "showOnPcHome": true },
     { "src": "images/my-work/6.jpg", "x": 50, "y": 50, "showOnMobileHome": false, "showOnPcHome": true },
     { "src": "images/my-work/7.jpg", "x": 70, "y": 100, "showOnMobileHome": false, "showOnPcHome": true },
     { "src": "images/my-work/12.jpg", "x": 70, "y": 50, "showOnMobileHome": false, "showOnPcHome": true },
-    { "src": "images/my-work/11.jpg", "x": 80, "y": 50, "showOnMobileHome": false, "showOnPcHome": true },
+    { "src": "images/my-work/11.jpg", "x": 80, "y": 50, "showOnMobileHome": false, "showOnPcHome": false },
     { "src": "images/my-work/10.jpg", "x": 50, "y": 50, "showOnMobileHome": false, "showOnPcHome": true },
-    { "src": "images/my-work/9.jpg", "x": 50, "y": 50, "showOnMobileHome": false, "showOnPcHome": true },
-    { "src": "images/my-work/14.jpg", "x": 60, "y": 50, "showOnMobileHome": false, "showOnPcHome": true },
+    { "src": "images/my-work/9.jpg", "x": 50, "y": 50, "showOnMobileHome": false, "showOnPcHome": false },
+    { "src": "images/my-work/14.jpg", "x": 60, "y": 50, "showOnMobileHome": false, "showOnPcHome": false },
     { "src": "images/my-work/8.jpg", "x": 60, "y": 50, "showOnMobileHome": false, "showOnPcHome": true },
-    { "src": "images/my-work/13.jpg", "x": 0, "y": 50, "showOnMobileHome": false, "showOnPcHome": true },
-    { "src": "images/my-work/15.jpg", "x": 0, "y": 50, "showOnMobileHome": false, "showOnPcHome": true }
+    { "src": "images/my-work/13.jpg", "x": 0, "y": 50, "showOnMobileHome": false, "showOnPcHome": false },
+    { "src": "images/my-work/15.jpg", "x": 0, "y": 50, "showOnMobileHome": false, "showOnPcHome": false }
   ],
   "generatedAt": "2026-06-06T15:52:00.000Z"
 };
